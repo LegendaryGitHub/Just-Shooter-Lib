00f5fb3545700b5c5c4fb2aef837fe05ea3cd8f1
Michael Wilde <techbub619@gmail.com>
2024-04-01 17:46:13 -0700

    Calcs are correct :)

---
+++ Home.java
@@ -0,0 +1,16 @@
+public class Home {
+    public static ShooterObject shooterObject = new ShooterObject();
+    public static void main(String[] args) {
+        // System.out.println(shooterObject.v0y);
+        // System.out.println(shooterObject.th);
+        // System.out.println("Vertical Reach Max Height" + shooterObject.xPositionAtMaxHeight());
+
+        // System.out.println("When the object will reach the max height it's location on the x-axis: " + shooterObject.xPositionAtMaxHeight());
+
+        // System.out.println("The max height the object will reach: " + shooterObject.distancehorizontal());
+
+        System.out.println("v0x: " + shooterObject.v0x);
+        System.out.println("td: " + shooterObject.td);
+        System.out.println("Check the Horizontal Distance" + shooterObject.distancehorizontal());
+    }
+}
